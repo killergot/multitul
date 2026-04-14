4db6c9a7c6bb22ad8b4ca88716f769bf2008c3ba
killergot <disney.132435morozko@mail.ru>
2026-04-14 17:15:53 +0300

    feat: add resolve and get refs

--- src/utils/git/repository.rs
+++ src/utils/git/repository.rs
@@ -3,6 +3,7 @@ use crate::utils::git::git_ref::GitRef;
 use crate::utils::git::hash::Hash;
 use crate::utils::git::ref_name::RefName;
 use std::collections::HashMap;
+use crate::utils::git::ref_target::RefTarget;
 
 #[derive(Debug, Default, Clone)]
 pub struct GitRepository {
@@ -15,4 +16,35 @@ impl GitRepository {
     pub fn new() -> Self {
         GitRepository::default()
     }
+
+    pub fn resolve_ref(&self, git_ref: GitRef) -> Option<Hash> {
+        match git_ref.target {
+            RefTarget::Direct(hash) => Some(hash),
+            RefTarget::Symbolic(name) => {
+                self.refs.get(&name)
+                    .and_then(|next| self.resolve_ref(next.clone()))
+            }
+        }
+    }
+
+    pub fn refs_by_hash(&self) -> HashMap<Hash, Vec<RefName>> {
+        let mut result: HashMap<Hash, Vec<RefName>> = HashMap::new();
+
+        for (name, git_ref) in self.refs.iter() {
+            if let Some(hash) = self.resolve_ref(git_ref.clone()) {
+                result.entry(hash).or_default().push(name.clone());
+            }
+        }
+
+        if let Some(head) = &self.head {
+            if let Some(hash) = self.resolve_ref(head.clone()) {
+                result.entry(hash).or_default().push(head.name.clone());
+            }
+        }
+
+        result
+
+    }
+
+
 }
